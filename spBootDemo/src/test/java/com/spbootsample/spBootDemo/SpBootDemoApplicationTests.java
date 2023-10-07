0b1886c7e5660a71c2a13cef0c87a111327774c6
Nacho González <nachohgon99@gmail.com>
2023-10-07 13:10:15 +0200

    primer commit bueno

---
+++ spBootDemo/src/test/java/com/spbootsample/spBootDemo/SpBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbootsample.spBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
